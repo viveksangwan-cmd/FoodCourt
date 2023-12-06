a7c1023c2f33469d20a109e75fa14fdb6bfd075a
viveksangwan-cmd <viveksangwanemail@gmail.com>
2023-12-06 17:01:12 +0530

    Initial Frontend
    Added initial frontend components.

---
+++ food-court-frontend/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Chicken Biryani','Rice with Chicken','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+}
